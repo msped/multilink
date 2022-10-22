52dd428b8a18b16ad92ce25e2cd2c689c984825a
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-22 19:03:33 +0100

    feat: added profile abstract user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Profile(AbstractUser):
+    profile_picture = models.ImageField(upload_to="profile_pictures/", null=True, blank=True)
+    bio = models.TextField(max_length=256, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.username}'s Profile"
