e3fca117e7783b8c637bccc88e8c1a25130d88ed
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-26 23:32:13 +0100

    feat: added createlink view

--- links/views.py
+++ links/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from accounts.models import Profile
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import (CreateAPIView,
+                                     RetrieveUpdateDestroyAPIView)
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Links, Networks
+from .serailizers import LinkSerializer, NetworkSerializer
+
+
+class CreateLink(CreateAPIView):
+    permission_classes = [IsAuthenticated,]
+    serializer_class = LinkSerializer
+    queryset = Links.objects.all()
+
+    def perform_create(self, serializer):
+        user = get_object_or_404(Profile, id=self.request.user.id)
+        serializer.save(user=user)
