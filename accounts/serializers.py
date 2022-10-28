eff2fa8f9e067cb5352f99d67a476eac92c241f6
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-29 00:39:17 +0100

    refactor: removed profile serializer from accounts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 
-from accounts.models import Profile
-
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
@@ -13,14 +11,3 @@ class ChangePasswordSerializer(serializers.Serializer):
         if attrs['old_password'] == attrs['new_password'] or attrs['old_password'] == attrs['new_password2']:
             raise serializers.ValidationError("New password(s) must be different.")
         return attrs
-
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = [
-            'first_name',
-            'last_name',
-            'username',
-            'profile_picture',
-            'bio'
-        ]
