ded4dfb53d8dad56c5597546b77fc362fd3da1f5
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-28 20:43:20 +0100

    feat: added editlink url

--- links/urls.py
+++ links/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CreateLink
+from .views import CreateLink, EditLink
 
 urlpatterns = [
     path('', CreateLink.as_view(), name="create_link"),
+    path('<int:pk>', EditLink.as_view(), name="edit_link")
 ]
