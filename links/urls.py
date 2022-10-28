a9dba027b6c8597d299b7d3455fec53d222c6bb1
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-28 22:00:53 +0100

    feat: added get networks url

--- links/urls.py
+++ links/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CreateLink, EditLink
+from .views import CreateLink, EditLink, GetNetworks
 
 urlpatterns = [
     path('', CreateLink.as_view(), name="create_link"),
-    path('<int:pk>', EditLink.as_view(), name="edit_link")
+    path('<int:pk>', EditLink.as_view(), name="edit_link"),
+    path('networks', GetNetworks.as_view(), name="get_networks"),
 ]
