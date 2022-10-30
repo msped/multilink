57ee18919d4b74015b681e8780c74faac64a49c7
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-30 18:14:43 +0000

    feat: added profile url to links url

--- links/urls.py
+++ links/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import CreateLink, EditLink, GetNetworks
+from .views import CreateLink, EditLink, GetNetworks, Profile
 
 urlpatterns = [
     path('', CreateLink.as_view(), name="create_link"),
     path('<int:pk>/', EditLink.as_view(), name="edit_link"),
     path('networks/', GetNetworks.as_view(), name="get_networks"),
+    path('<str:username>/', Profile.as_view(), name="profile"),
 ]
