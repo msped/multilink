fe037c0280fb8789960073d7c8bdb4b3175393b4
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-31 22:55:34 +0000

    fix: changed profile.as_view() to profileview.as_view()

--- links/urls.py
+++ links/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import CreateLink, EditLink, GetNetworks, Profile
+from .views import CreateLink, EditLink, GetNetworks, ProfileView
 
 urlpatterns = [
     path('', CreateLink.as_view(), name="create_link"),
     path('<int:pk>/', EditLink.as_view(), name="edit_link"),
     path('networks/', GetNetworks.as_view(), name="get_networks"),
-    path('<str:username>/', Profile.as_view(), name="profile"),
+    path('<str:username>/', ProfileView.as_view(), name="profile"),
 ]
