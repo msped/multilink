80315ef969fe708b9d828e7947dc83f94285d34b
Matt Edwards <mattsp.edwards@googlemail.com>
2022-11-12 23:18:22 +0000

    feat: added new token url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from .views import BlacklistTokenView, ChangePasswordView
+from .views import BlacklistTokenView, ChangePasswordView, CustomTokenObtainPairView
 
 urlpatterns = [
     path('', include('djoser.urls.jwt')),
     path('', include('djoser.urls')),
+    path('jwt/token/', CustomTokenObtainPairView.as_view(), name="obtain_token_pair"),
     path('jwt/blacklist/', BlacklistTokenView.as_view(), name="logout"),
     path(
         'change-password/',
